2b299e63e8cdad299545648e2b860601c10fcc54
mainanorbert <mainanorbert@gmail.com>
2023-09-22 13:30:50 +0000

    betty compliant

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,32 +1,30 @@
 #include "sort.h"
-
 /**
  * bubble_sort - sorts an array
  * @array: Array to sort.
  * @size: size of array.
  * Return: Void.
  */
-
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp, flag;
+	size_t i, j;
+	int temp, flag;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        flag = 0;
-        for (j = 0; j < size-1-i; j++)
-        {
-            if (array[j] > array[j+1])
-            {
-                temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-                flag =1;
-                print_array(array,size);
-            }
-        }
-        if (flag == 0)
-            break;
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (flag == 0)
+			break;
+	}
 }
