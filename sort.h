78b95fa6b74f0fc9f26a82eadcc61c4c2c79dd6b
mainanorbert <mainanorbert@gmail.com>
2023-09-30 09:39:00 +0000

    implementing counting sort

--- sort.h
+++ sort.h
@@ -24,4 +24,5 @@ void quick_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 #endif
